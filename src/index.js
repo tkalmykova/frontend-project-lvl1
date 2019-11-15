bee2f10fa3664ac9a7fe5758fdca569967f30511
Tatiana Kalmykova <ul.tkalmykova@gmail.com>
2019-11-15 21:01:46 +0400

    add eslint

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import readlineSync from 'readline-sync';
-export const greetUser = () => {
+
+const greetUser = () => {
   console.log('Welcome to the Brain Games!');
   console.log();
 
   const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}!`)
-}
+  console.log(`Hello, ${name}!`);
+};
+
+export default greetUser;
