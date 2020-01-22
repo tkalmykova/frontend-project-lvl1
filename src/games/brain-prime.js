3a5d2d4a021540698e68c4b58db604c1fa928cf0
Tatiana Kalmykova <ul.tkalmykova@gmail.com>
2020-01-22 20:39:30 +0400

    optimize isPrime function

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -2,7 +2,7 @@ import { cons } from '@hexlet/pairs';
 import { getRandomNumber } from '../utils';
 
 const isPrime = (num) => {
-  for (let i = 2; i < num; i += 1) {
+  for (let i = 2; i <= num / 2; i += 1) {
     if (num % i === 0) {
       return false;
     }
