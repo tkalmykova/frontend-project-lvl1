2fc27541a2a6cb560cdccdadfa6d9c43e7833604
Tatiana Kalmykova <ul.tkalmykova@gmail.com>
2020-02-08 19:47:08 +0400

    add conditions for negative numbers, 0 and 1 to the brain-prime

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -3,6 +3,9 @@ import getRandomNumber from '../utils';
 import createGame from '../engine';
 
 const isPrime = (num) => {
+  if (num <= 1) {
+    return false;
+  }
   for (let i = 2; i <= num / 2; i += 1) {
     if (num % i === 0) {
       return false;
